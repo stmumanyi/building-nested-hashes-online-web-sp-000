15cce64bbc8a2d841acb48a43c998efbff6829d3
Simba Mumanyi <simbamumanyi@gmail.com>
2020-11-10 16:03:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,8 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague"
+        :age => 
         },
       :matriarch => {
         #your key/value pairs here
