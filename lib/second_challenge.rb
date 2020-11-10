7bea7111184148312ff30903d6cec3574e1994da
Simba Mumanyi <simbamumanyi@gmail.com>
2020-11-10 15:58:51 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,10 @@ def second_challenge
       
       }, 
     :capulet => {
-      
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      heroine_friends => []
       }}
 
   
